0413e6f494aca08a5d33344cfb61ff38ffac14be
bububebe0 <63367673+bububebe0@users.noreply.github.com>
2026-05-06 15:31:00 +0500

    Update ui.cs

--- ui.cs
+++ ui.cs
@@ -392,3 +392,11 @@
         return btn;
     }
 }
+
+    private static readonly string _titleBtnXaml = @"
+<ControlTemplate TargetType='Button' xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation' xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'>
+  <Border x:Name='bd' Background='{TemplateBinding Background}'>
+    <TextBlock Text='{TemplateBinding Content}' HorizontalAlignment='Center' VerticalAlignment='Center'
+               Foreground='{TemplateBinding Foreground}' FontSize='12' FontWeight='Normal'/>
+  </Border>
+</ControlTemplate>";
